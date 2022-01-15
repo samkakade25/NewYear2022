da828633d935531f9c8c5a2f4a090d9451fb7609
Sameer Kakade <samkakade44@gmail.com>
2022-01-15 10:37:35 +0530

    Main file added

---
+++ src/com/Sameer/Main.java
@@ -0,0 +1,14 @@
+package com.Sameer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+       // System.out.println("Hello World none of the above  " + number(25));
+        System.out.println(number(25));
+    }
+           static int number(int x) {
+         // x=(25);
+          return x;
+           }
+}
