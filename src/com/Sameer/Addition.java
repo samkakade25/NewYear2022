3c9be63a02fccf85103f52b43182f4d2a95a9c52
Sameer Kakade <samkakade44@gmail.com>
2022-01-15 10:56:14 +0530

    New file added

---
+++ src/com/Sameer/Addition.java
@@ -0,0 +1,19 @@
+package com.Sameer;
+
+public class Addition {
+    public static void main(String[] args) {
+        //int a = 20;
+       // int b = 30;
+        //int ans = a+b;
+        major();
+        System.out.println(major());
+    }
+    static int major(){
+
+
+    int a = 20;
+    int b = 30;
+    int ans = a+b;
+    return ans;
+    }
+}
