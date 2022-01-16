10333630b30510a596a83fe47fad77fb3b5c7b0a
Sameer Kakade <samkakade44@gmail.com>
2022-01-16 13:29:34 +0530

    new file added

---
+++ src/com/Sameer/Greeting.java
@@ -0,0 +1,12 @@
+package com.Sameer;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+
+    greeting();
+}
+      static void greeting() {
+          System.out.println("Hiii am a");
+      }
+}
