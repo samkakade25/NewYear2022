137505dff02032e5706d1ef640a06585b7a6a073
Sameer Kakade <samkakade44@gmail.com>
2022-01-21 18:54:53 +0530

    new branch

---
+++ src/com/Sameer/Comparison.java
@@ -0,0 +1,12 @@
+package com.Sameer;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a = "Sameer";
+        String b = "Sameer";
+        String c = a;
+        System.out.println(b == c);
+    }
+}
+
+//    public static void main(String[] args)  {  }
